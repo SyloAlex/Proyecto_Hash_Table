932deb5ffc2a8d919cdb1d290ca45f01342c0a07
Luis Manzur <luismazur91@outlook.com>
2021-11-24 09:46:53 -0400

    txt-reading

--- src/proyecto_hash_table/proyecto_hash_table.java
+++ src/proyecto_hash_table/proyecto_hash_table.java
@@ -5,6 +5,8 @@
  */
 package proyecto_hash_table;
 
+import txt.TxtReader;
+
 /**
  *
  * @author luism
@@ -15,7 +17,6 @@ public class proyecto_hash_table {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
